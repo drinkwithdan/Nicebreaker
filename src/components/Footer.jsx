f040938d567184257ad33275df76652390475934
Dan S <drinkwithdan@gmail.com>
2022-10-05 15:47:52 +1100

    Init commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h6>This is the footer</h6>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div className="navbar" >
+      <h1>Nicebreaker</h1>
+      <h3>Find the nicest icebreaker</h3>
+    </div>
+  )
+}
+
+export default Navbar
