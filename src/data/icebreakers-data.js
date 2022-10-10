6e90415a9b43e77a363287ab5d947495d88c4b79
Dan S <drinkwithdan@gmail.com>
2022-10-10 17:14:08 +1100

    Data changes

--- src/data/icebreakers-data.js
+++ src/data/icebreakers-data.js
@@ -1,23 +1,33 @@
 const icebreakers = [
   {
     id: 1,
-    title: "Speed Portrait Drawing",
+    title: "Meet-and-greet Portraiture",
     minPeople: 2,
     maxPeople: 100,
     preparation: "Some",
     online: true,
     inPerson: true,
-    instructions: "Everyone pairs up and has 1 - 3 minutes to draw their partner whilst simultaneously finding out about them, and incorporating that into their drawing (can give specifics- like what they wanted to be when they were kids etc)"
+    instructions: `Everyone pairs up and has 1-3 minutes each to learn about their partner, while simultaneously drawing their portrait. Facts learned can be incorporated into their picture and the outcome is a ‘get to know you’ gallery. Each person then shows their masterpiece and introduces their partner to the group. 
+    
+    This exercise can also be themed for the subject of your workshop. For example if you’re session is on careers, the interview theme could be about what they wanted to be when they grew up. The portraits all then are based on a theme of careers. If running this activity online, allocate random pairs to break-out rooms then come back together for the introductions. 
+    
+    You can also have group members take photos of their portraits and add them to a miro board or google drive folder.`
   },
   {
     id: 2,
-    title: "Interview and Introduce",
+    title: "Introduce someone else",
     minPeople: 2,
-    maxPeople: 100,
+    maxPeople: 30,
     preparation: "Some",
     online: true,
     inPerson: true,
-    instructions: "Everyone pairs up and has 1 - 5 minutes to interview their partner. At the end of this time each candidate introduces their partner to the rest of the group."
+    instructions: `A great one for avoiding the anxiety of having to introduce yourself- get someone else to do it!
+
+    Simply pair everyone up, and ask them to find out a bit about each other for 5-10 minutes. Then, go around the pairs and have everyone introduce their partner!
+    
+    It helps the pairs get going if you have some questions for them to start with. Write these on a whiteboard or have them on a slide. 
+    
+    If running this activity online, allocate random pairs to break-out rooms then come back together for the introductions.`
   },
   {
     id: 3,
@@ -27,7 +37,11 @@ const icebreakers = [
     preparation: "None",
     online: true,
     inPerson: true,
-    instructions: "Each person takes turns introducing themselves with 3 statements, the rest of the group has to decide which is the lie."
+    instructions: `Split the participants into small groups of 4 to 6 people.
+
+    Ask each person in the group to write three statements about themselves: 2 true and 1 false. Explain that each person, in turn, will need to share their three statements with the rest of the group, who will have to guess which statement is the false one.
+    
+    After the first person has shared their statement and the group has decided which statement is false, the first person will reveal the truth. Move on until each person in the group has shared their statements.`
   },
   {
     id: 4,
@@ -37,7 +51,9 @@ const icebreakers = [
     preparation: "Some",
     online: false,
     inPerson: true,
-    instructions: "Everyone tells two truths and a lie through drawings- one per post-it note - which they use to build a gallery on the wall. Everyone then votes for which one they think is a lie."
+    instructions: `Ask each person in the group to use post-its to capture 2 truths and 1 lie about themselves- using drawings! Put all the post-its up on the wall, and ask each person, in turn, to share their three sketches with the rest of the group, who will have to guess which is the false one.
+
+    After the first person has shared their statement and the group then uses sticky dots to vote for which one they think is a lie.`
   },
   {
     id: 5,
@@ -47,7 +63,9 @@ const icebreakers = [
     preparation: "Lots",
     online: false,
     inPerson: true,
-    instructions: "On entry, everyone is given a card with an item of food on it, which belongs to a group of foods - eg. types of cheese, canned food, cocktails or hipster foods. Everyone must then find their teams by speaking to everyone else. Teams then have to name themselves."
+    instructions: `Everyone in the workshop is given a post-it or card with an item of food on it. They must then go around the room and identify their team members by finding foods which fall into a recognisable category. For example, people might be types of cheese, canned foods, cocktails or health food. Each group then gives themselves a name for the rest of the session.
+
+    Food items can be handed out at random, or you can plan teams in advance (though this increases the amount of prep to do!).`
   },
   {
     id: 6,
@@ -57,17 +75,40 @@ const icebreakers = [
     preparation: "Lots",
     online: true,
     inPerson: true,
-    instructions: "Fill a hat full of random words and phrases which participants have to select 2 from. They then have 5 minutes to generate as many product or service ideas from this combo as possible. Can host online using a Miro board with hidden form fields instead. Depending on number of participants and time available, everyone can share back at the end or facilitator can ask for a few examples people came up with."
+    instructions: `Fill a hat full of random words and phrases which participants have to select 2 from. 
+
+    Words could include things like:
+    
+    Sheep
+    Umbrella
+    Diary
+    Shop
+    Astronaut
+    Cloud
+    Spaghetti
+    Taxi
+    
+    and anything else you want!
+    
+    Participants then have 5 minutes to generate as many product, business or service ideas from their combo as possible! No idea is too silly. 
+    
+    Depending on number of participants and time available, everyone can share back at the end or facilitator can ask for a few examples people came up with.
+    
+    If you’re running this activity online, you can use a word randomiser website (although people can cheat that way!) or using a Miro board with words hidden behind objects instead. `
   },
   {
     id: 7,
-    title: "Randomised Story-telling",
+    title: "Storytime",
     minPeople: 2,
     maxPeople: 100,
     preparation: "Lots",
     online: true,
     inPerson: true,
-    instructions: "Participants pull 5 symbol cards at random from a hat, than have to use them to tell a story about their day/a recent holiday/ a fairy tale etc."
+    instructions: `Fill a hat full of cards or post-its that have simple icons / pictures on them. Participants pull 5 symbol cards each at random, than have to use them to tell a story. This story could be random, or you could make it on theme with the session you’re running.
+
+    Depending on number of participants and time available, everyone can share back at the end or facilitator can ask for a few examples people came up with.
+    
+    If you’re running this activity online, you can use a word randomiser website (although people can cheat that way!) or using a Miro board with icons hidden behind objects instead.`
   },
   {
     id: 8,
@@ -77,7 +118,11 @@ const icebreakers = [
     preparation: "None",
     online: true,
     inPerson: true,
-    instructions: "Once in teams participants have 5 minutes to find two things they all have in common to name their team after (eg we all drink lots of coffee and have owned a rabbit)."
+    instructions: `Break the group into teams of 3-6 people. 
+
+    Once in teams, participants have 5-15 minutes to find two things they all have in common. This could be interests or hobbies, places they live, pets they own, experiences they’ve had etc. 
+    
+    Once the have identified 2 things everyone has in common, they can use these to name their team after (eg we all drink lots of coffee and have owned a rabbit).`
   },
   {
     id: 9,
@@ -87,7 +132,37 @@ const icebreakers = [
     preparation: "Lots",
     online: true,
     inPerson: true,
-    instructions: "Participants answer yes, no or maybe questions by physically moving to different parts of the room where facilitators are holding up answers. Can be great to get people energised, give a sense of the types of people in the room and give framing for the conversations you want to have in the rest of the workshop."
+    instructions: `Active data helps a room full of people get to know each other in a quick and active way.
+
+    Have two answer stations at each side of the room (or two ends of a scale on a miro board). The facilitator then asks a series of questions with binary answers, and allocates one side of the room or the other to a different answer. 
+    
+    For example, you might ask the room, “are you a cat or a dog person?” then allocate the left of the room to the cats and the right to the dogs.
+    
+    Participants then answer the questions by physically moving to different parts of the room where facilitators have allocated the answers (you can even have them holding up signs if you like).  If the answer is neither, both or somewhere in between, people can stand somewhere along the middle between points. It’s fun to then pick on participants to enquire about their answer, to get the conversation going in a fun and unpressured way.
+    
+    Other questions you could use:
+    Do you prefer pizza or pasta?
+    Are you a morning or a night person?
+    Sweet or Savoury?
+    Books or Movies?
+    
+    You can also tailor the questions to the subject of the workshop if you like.
+    
+    This is a great one to get people energised, give a sense of the types of people in the room and give framing for the conversations you want to have in the rest of the workshop. However, it is not great if you have participants with mobility challenges, in which case you might want to rethink and use a ‘hands up’ system instead.`
+  },
+  {
+    id: 10,
+    title: "Rock Paper Scissor Tournament",
+    minPeople: 4,
+    maxPeople: 100,
+    preparation: "None",
+    online: false,
+    inPerson: true,
+    instructions: `A simple but classic warm up to get people playing and talking. 
+
+    Get your participants into pairs, and invite them to play 3 rounds of ‘rock, paper, scissors’. Whoever wins, goes on to play another round with another winner, while the losing player goes to sit back down.
+    
+    This continues until finally two players remain.`
   },
 
 ]
