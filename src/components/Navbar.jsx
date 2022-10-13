6f00489b7dda63f156a88958d509bdd9254bf98f
Dan S <drinkwithdan@gmail.com>
2022-10-13 12:41:15 +1100

    Styling changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ const Navbar = () => {
     <div className="navbar" >
       <h1>Nicebreaker</h1>
       <h3>Search for a pre-workshop Icebreaker</h3>
+      <p>(I'm still being built, please be gentle!)</p>
     </div>
   )
 }
