022a8d039a7add3d1f14c7560118e3bc186d67ff
Dan S <drinkwithdan@gmail.com>
2022-10-14 15:20:55 +1100

    Styling changes, formatting changes

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -3,21 +3,13 @@ import { Link } from "react-router-dom"
 const Card = ({ card }) => {
   return (
     <div className="card">
-      <Link style={{ textDecoration: 'none', color: "black" }} to={`/show/${card.id}`}>
-        <h3 className="card-title">{card.title}</h3>
-        <div className="card-body">
+      <Link className="card-link" style={{ textDecoration: 'none', color: "black" }} to={`/show/${card.id}`}>
+        <div className="card-title">
+          <h3 className="card-title">{card.title}</h3>
+        </div>
 
-          <p>{card.minPeople} - {card.maxPeople} participants.</p>
-          <p>
-            {
-              ((card.online && card.inPerson) && "Online and in-person")
-              || (card.online && "Online")
-              || (card.inPerson && "In-person")
-            }
-          </p>
-
+        <div className="card-prep">
           <p>Preparation: {card.preparation}</p>
-
         </div>
       </Link>
     </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
   return (
     <div className="footer">
       <h6>© ScoDougall Enterprises 2022 | <a href="https://github.com/drinkwithdan" target="_blank">Github</a> | <a href="https://code-with-dan.com" target="_blank" >Portfolio</a></h6>
+      <h6>Made with respect on unceded Wurundjeri land</h6>
     </div>
   )
 }
